1eec4a768c6a01e1c90cbb4e8ed4244b42ed6a0b
Ab-Ruh <abell.demere@gmail.com>
2023-08-30 16:05:40 +0300

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,10 +12,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 if (tree == NULL)
 return (0);
 
-/* Recursively calculate the size of the left and right subtrees */
-size_t left_size = binary_tree_size(tree->left);
-size_t right_size = binary_tree_size(tree->right);
-
 /* Add 1 for the current node */
-return (1 + left_size + right_size);
+return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
