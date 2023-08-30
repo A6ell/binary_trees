11928941805e3b5e0d32a02eeda503ae34a57e55
Ab-Ruh <abell.demere@gmail.com>
2023-08-30 16:35:11 +0300

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,28 +10,30 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
 
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
+	if (!tree->right && !tree->left)
 		return (1);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
 
-	if (left_height == right_height)
-		return (1);
-	return (0);
+	power = (size_t)_pow_recursion(2, height + 1);
+	return (power - 1 == nodes);
 }
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: the value to exponentiate
- * @y: the power to raise x to
- * Return: x to the power of y, or -1 if y is negative
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: the value to exponentiate
+ *@y: the power to raise x to
+ *Return: x to the power of y, or -1 if y is negative
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -40,6 +42,7 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
+
 }
 
 /**
@@ -66,13 +69,13 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (!tree)
 		return (0);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (left_height > right_height ? left_height : right_height);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
