8bfe899174dfc50e8d042a8549ec156866e67510
Ab-Ruh <abell.demere@gmail.com>
2023-08-30 16:53:07 +0300

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,9 +24,9 @@ int _pow_recursion(int x, int y)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = 0;
-	size_t nodes = 0;
-	size_t power = 0;
+	size_t level = 0;
+	size_t count = 0;
+	size_t exponent = 0;
 
 	if (!tree)
 		return (0);
@@ -34,11 +34,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	height = binary_tree_height(tree);
-	nodes = binary_tree_size(tree);
+	level = binary_tree_height(tree);
+	count = binary_tree_size(tree);
 
-	power = (size_t)_pow_recursion(2, height + 1);
-	return (power - 1 == nodes);
+	exponent = (size_t)_pow_recursion(2, level + 1);
+	return (exponent - 1 == count);
 }
 
 /**
