caeed889530be1ce417f14d00f478d41ea24304d
Ab-Ruh <abell.demere@gmail.com>
2023-08-30 15:44:21 +0300

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,14 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height = 0;
+size_t left_height, right_height;
 
-if (!tree)
+if (tree == NULL)
 return (0);
 
 left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 /* Return the larger of the two subtrees */
-return (1 + (left_height > right_height ? left_height : right_height));
+return (left_height > right_height ? left_height : right_height);
 }
