eed19b8638abfe5ea3f603a95884f50b0e83249e
Ab-Ruh <abell.demere@gmail.com>
2023-08-30 16:47:07 +0300

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,16 +17,16 @@ int _pow_recursion(int x, int y)
 }
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: Pointer to the root node of the tree to check.
- *
- * Return: 1 if the tree is perfect, 0 otherwise. If tree is NULL, return 0.
- */
+* binary_tree_is_perfect - Checks if a binary tree is perfect.
+* @tree: Pointer to the root node of the tree to check.
+*
+* Return: 1 if the tree is perfect, 0 otherwise. If tree is NULL, return 0.
+*/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_subtree_height = 0;
-	size_t total_nodes = 0;
-	size_t expected_nodes = 0;
+	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
 
 	if (!tree)
 		return (0);
@@ -34,14 +34,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	left_subtree_height = binary_tree_height(tree->left);
-	total_nodes = binary_tree_size(tree);
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
 
-	expected_nodes = (size_t)_pow_recursion(2, left_subtree_height + 1);
-
-	return (expected_nodes == total_nodes);
+	power = (size_t)_pow_recursion(2, height + 1);
+	return (power - 1 == nodes);
 }
-
 
 /**
 * binary_tree_size - Measures the size of a binary tree.
